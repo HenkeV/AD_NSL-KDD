3994fc35ead2ad90242019d382fc47666b77ee6d
HenkeV <henrik.volden@gmail.com>
2016-04-05 17:19:54 +0200

    Started on knn
    Initial tests knn

--- test.R
+++ test.R
@@ -1,3 +1,9 @@
-kdd=read.csv(file="KDDTest+.arff", sep = ",")
+kdd_train=read.csv(file="KDDTrain+.arff", sep = ",")
+kdd_test=read.csv(file="KDDTrain+_20Percent.arff", sep = ",")
 colnames <- read.table("names", skip = 1, sep = ":")
-names(kdd) <- colnames$V1
+names(kdd_train) <- colnames$V1
+names(kdd_test) <- colnames$V1
+train <- (kdd_train$class)
+test <- (kdd_test$class)
+require(class)
+knn(train = kdd_test, test = kdd_train, cl = class, k = 355)
